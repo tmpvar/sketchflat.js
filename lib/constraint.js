tmpvar/sketchflat.js

commit 54a89b932b88cab48084ed596f9a2d7b9267979f

allow distance to be based on reference entities

lib/constraint.js
@@ -75,7 +75,7 @@ Constraint.createDistanceFromPointToLine = function(point, line) {
 // * point - line
 // * line - point
 // * point - point
-Constraint.createDistance = function(v1, v2) {
+Constraint.createDistance = function(v1, v2, v1ref, v2ref) {
 
   // line and line
   if (v1.end && v2.end) {
@@ -89,16 +89,38 @@ Constraint.createDistance = function(v1, v2) {
     return Constraint.createDistanceFromPointToLine(v2, v1);
 
   } else {
+    var v1x = Param.fromObject(v1, 'x');
+    var v1y = Param.fromObject(v2, 'y');
+    if (v1ref) {
+      v1x.reference = true;
+      v1x.known = true;
+      v1y.reference = true;
+      v1y.known = true;
+    }
+
+    var v2x = Param.fromObject(v2, 'x');
+    var v2y = Param.fromObject(v2, 'y');
+
+    if (v2ref) {
+      v2x.reference = true;
+      v2x.known = true;
+      v2y.reference = true;
+      v2y.known = true;
+    }
 
     return eOp('sqrt',
       eOp('+',
-        eOp('square', eOp('-',
-          eParam(Param.fromObject(v1, 'x')),
-          eParam(Param.fromObject(v2, 'x')))
+        eOp('square',
+          eOp('-',
+            eParam(v1x),
+            eParam(v2x)
+          )
         ),
-        eOp('square', eOp('-',
-          eParam(Param.fromObject(v1, 'y')),
-          eParam(Param.fromObject(v2, 'y')))
+        eOp('square',
+          eOp('-',
+            eParam(v1y),
+            eParam(v2y)
+          )
         )
       )
     );

commit f3741de216f7ec0f2c0d90881904fb2e62f13fc0

fix issue introduced by 54a89b932

lib/constraint.js
@@ -90,7 +90,8 @@ Constraint.createDistance = function(v1, v2, v1ref, v2ref) {
 
   } else {
     var v1x = Param.fromObject(v1, 'x');
-    var v1y = Param.fromObject(v2, 'y');
+    var v1y = Param.fromObject(v1, 'y');
+
     if (v1ref) {
       v1x.reference = true;
       v1x.known = true;